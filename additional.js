8258662fd3132f1778e5c20e3e427d49c3ccc7b7
DineshShankar <dineshshankar156@gmail.com>
2024-07-18 21:12:55 +0530

    c2

---
+++ additional.js
@@ -0,0 +1,11 @@
+var typed = new Typed("#typed-output", {
+    strings: ["Web Designer", "Front-End developer", "Enjoy your stay!"],
+    typeSpeed: 50,
+    backSpeed: 25,
+    backDelay: 1000,
+    startDelay: 500,
+    loop: true,
+    showCursor: true,
+    cursorChar: "",
+    autoInsertCss: true,
+  });
