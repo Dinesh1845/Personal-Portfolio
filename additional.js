1211e78957740b234e96b6a9b4c64d0bef6fe194
DineshShankar <dineshshankar156@gmail.com>
2024-07-19 21:52:33 +0530

    added mouse movement code

--- additional.js
+++ additional.js
@@ -1,5 +1,5 @@
 var typed = new Typed("#typed-output", {
-    strings: ["Web Designer", "Front-End developer", "Enjoy your stay!"],
+    strings: ["Web Designer", "Front-End developer", "Back-End developer"],
     typeSpeed: 50,
     backSpeed: 25,
     backDelay: 1000,
@@ -9,3 +9,22 @@ var typed = new Typed("#typed-output", {
     cursorChar: "|",
     autoInsertCss: true,
   });
+
+
+
+  let inner = document.getElementById("inner");
+  let outer = document.getElementById("outer");
+
+  window.addEventListener("mousemove", (e) => {
+    inner.style.left = `${e.clientX}px`
+    inner.style.top = `${e.clientY}px`
+    
+    outer.animate({
+      left : `${e.clientX}px`,
+      top : `${e.clientY}px`
+    }, {duration:500 , fill:"forwards"})
+  });
+
+  let files = document.getElementById("files");
+
+  
