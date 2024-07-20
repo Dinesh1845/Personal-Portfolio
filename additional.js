70a692c250899c9727e6e53bb881a12ced6f2824
DineshShankar <dineshshankar156@gmail.com>
2024-07-20 21:38:54 +0530

    this branch is for testing coeds

--- additional.js
+++ additional.js
@@ -1,23 +1,3 @@
-var typed = new Typed("#typed-output", {
-    strings: ["Web Designer", "Front-End developer", "Back-End developer"],
-    typeSpeed: 50,
-    backSpeed: 25,
-    backDelay: 1000,
-    startDelay: 500,
-    loop: true,
-    showCursor: true,
-    cursorChar: "|",
-    autoInsertCss: true,
-  });
-
-let pointer = document.getElementById("pointer");
-
-window.addEventListener("mousemove" , (e)=>{
-  pointer.animate({
-    left : `${e.clientX}px`,
-    top : `${e.clientY}px`
-  }, {duration:500 , fill:"forwards"})
-})
 
 
   
