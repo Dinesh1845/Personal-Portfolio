f2925222a02785d7fcc2889ef4b4ba27ea1edce7
DineshShankar <dineshshankar156@gmail.com>
2024-07-21 13:35:07 +0530

    added scrollreval animation

--- additional.js
+++ additional.js
@@ -10,6 +10,8 @@ var typed = new Typed("#typed-output", {
     autoInsertCss: true,
   });
 
+
+//mouse follow
 let pointer = document.getElementById("pointer");
 
 window.addEventListener("mousemove" , (e)=>{
@@ -17,7 +19,15 @@ window.addEventListener("mousemove" , (e)=>{
     left : `${e.clientX}px`,
     top : `${e.clientY}px`
   }, {duration:500 , fill:"forwards"})
+});
+
+//scroll reveal
+ScrollReveal().reveal(".main" , {
+  duration:1000,
+  reset:true,
+  easing:'ease-in-out',
+  delay:500,
+  distance:'50px',
+  mobile:false,
+  origin:'top'
 })
-
-
-  
